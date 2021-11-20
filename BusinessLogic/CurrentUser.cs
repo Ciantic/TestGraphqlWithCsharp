d46b40a94993e6de582aaf1cf049f0c0eec48bb5
Jari Pennanen <ciantic@oksidi.com>
2021-11-20 23:47:40 +0200

    .

--- BusinessLogic/CurrentUser.cs
+++ BusinessLogic/CurrentUser.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 
 public partial class BusinessLogic
 {
+    [Authorize]
     public async Task<AppUser?> CurrentUser([CurrentUser] CurrentUser user)
     {
         return await user.User;
